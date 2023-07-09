00ac0c5ba4dbacb9c6f38b28c5459c6aabf6197c
Elena <elenela@yandex.ru>
2023-07-10 00:16:23 +0300

    ДЗ 11

--- src/main/java/com/pro/sky/HW11_spring/Hw11SpringApplication.java
+++ src/main/java/com/pro/sky/HW11_spring/Hw11SpringApplication.java
@@ -1,13 +1,9 @@
 package com.pro.sky.HW11_spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Hw11SpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Hw11SpringApplication.class, args);
 	}
-
 }
